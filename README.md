4d6cee07402a611f5f30d8553928ee79a7bb94a1
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-03-01 21:25:28 +0200

    about me project; reate a guessing game about my self main file index.html

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+# Code 201
 # About-me-project
+# Bayan ALkhatib
+# Lab-02:
+## Comments:
+     - this is the first lab; so 1/Mar is the date of creation of this repo.
+     - 
+# links 
+  - [.eslintrc.json file](https://github.com/LTUC/amman-201d18/blob/main/class-02/demo/.eslintrc.json)
+  - [reset.css file](https://meyerweb.com/eric/tools/css/reset/)
+  - [codefellows style guide](https://codefellows.github.io/code-201-guide/curriculum/class-02/project_setup)
+
+
---
+++ js/app.js
@@ -0,0 +1,65 @@
+'use strict'
+let myName='bayan';
+let favcolor='blue';
+let favsinger='sia';
+let myfriend='majdoleen';
+let mybarthday='may,17';
+
+let userName=prompt('please insert your name');
+    alert('welcome '+userName+' to our website');
+    alert('this game is for you to guess about me; please answer with yes or no');
+
+let theName=prompt('My name is lama');
+if(theName.toLowerCase() ==='no' || theName.toLowerCase() ==='n'){
+     alert( ' congrates your answer is correct' );
+     console.log (' congrates your answer is correct');
+     document.getElementById("name").innerText=('my name is bayan  your answer is correct');
+}else{
+    alert('your answer is wrong');
+    console.log('your answer is wrong');
+    document.getElementById("name").innerText=('my name is bayan  your answer isn ot correct');
+}
+let color=prompt('my favourite color is pink');
+ if(color.toLowerCase() ==='no' || color.toLowerCase() ==='n'){
+     alert(' congrates your answer is correct' );
+      console.log (' congrates your answer is correct');
+      document.getElementById("color").innerText=('my favourite color is blue  your answer is correct');
+}else{
+     alert('your answer is wrong');
+     console.log('your answer is wrong');
+     document.getElementById("color").innerText=('my favourite color is blue  your answer is not correct');
+    
+}
+let singer=prompt('my favourite singer is sia');
+if( singer.toLowerCase()==='yes' || singer.toLowerCase() ==='y'){
+     alert(' congrates your answer is correct' );
+      console.log (' congrates your answer is correct');
+    document.getElementById("singer").innerText=('my favourite singer is sia  your answer is correct');
+}else{
+    alert('your answer is wrong');
+     console.log('your answer is wrong');
+     document.getElementById("singer").innerText=('my favourite singer is sia your answer is not correct');
+}
+let friend=prompt('sama is my friend ');
+ if( friend.toLowerCase() ==='no' || friend.toLowerCase() ==='n'){
+      alert(' congrates your answer is correct' );
+     console.log (' congrates your answer is correct');
+     document.getElementById("friend").innerText=('my friend is majdoleen  your answer is correct');
+}else{
+    alert('your answer is wrong');
+    console.log('your answer is wrong');
+    document.getElementById("friend").innerText=('my friend is majdoleen  your answer is not correct');
+}
+let barthday=prompt('my barthday is may,17 ');
+if( barthday.toLowerCase()==='yes' || barthday.toLowerCase()==='y'){
+     alert( ' congrates your answer is correct' );
+     console.log ('congrates your answer is correct');
+     document.getElementById("barthday").innerText=('my barthday is may,17  your answer is correct');
+}else{
+    alert('your answer is wrong');
+    console.log('your answer is wrong');
+    document.getElementById("barthday").innerText=('my barthday is may,17  your answer is not correct');
+}
+
+alert (userName+' hope you enjoyed playing with us ')
+
