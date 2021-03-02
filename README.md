1f556eb4b08bc9433d0c959055f98d70ee2cc244
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-03-02 22:32:30 +0200

    modification about the game; adding Q 6&7, adding score

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 # Bayan ALkhatib
 # Lab-02:
 ## Comments:
-     - this is the first lab; so 1/Mar is the date of creation of this repo.
-     - 
+     - lab-02 first lab for this project ; so 1/Mar is the date of creation of this repo.
+     - lab-03 modification for the game; arrays and loops,adding Q 6 & 7, adding score .
 # links 
   - [.eslintrc.json file](https://github.com/LTUC/amman-201d18/blob/main/class-02/demo/.eslintrc.json)
   - [reset.css file](https://meyerweb.com/eric/tools/css/reset/)
--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let favcolor='blue';
 let favsinger='sia';
 let myfriend='majdoleen';
 let mybarthday='may,17';
+let score=0;
 
 let userName=prompt('please insert your name');
     alert('welcome '+userName+' to our website');
@@ -13,6 +14,7 @@ let theName=prompt('My name is lama');
 if(theName.toLowerCase() ==='no' || theName.toLowerCase() ==='n'){
      alert( ' congrates your answer is correct' );
      //console.log (' congrates your answer is correct');
+     score+=1;
      document.getElementById("name").innerText=('my name is bayan  your answer is correct');
 }else{
     alert('your answer is wrong');
@@ -23,6 +25,7 @@ let color=prompt('my favourite color is pink');
  if(color.toLowerCase() ==='no' || color.toLowerCase() ==='n'){
      alert(' congrates your answer is correct' );
       //console.log (' congrates your answer is correct');
+      score+=1;
       document.getElementById("color").innerText=('my favourite color is blue  your answer is correct');
 }else{
      alert('your answer is wrong');
@@ -34,6 +37,7 @@ let singer=prompt('my favourite singer is sia');
 if( singer.toLowerCase()==='yes' || singer.toLowerCase() ==='y'){
      alert(' congrates your answer is correct' );
       //console.log (' congrates your answer is correct');
+      score+=1;
     document.getElementById("singer").innerText=('my favourite singer is sia  your answer is correct');
 }else{
     alert('your answer is wrong');
@@ -44,6 +48,7 @@ let friend=prompt('sama is my friend ');
  if( friend.toLowerCase() ==='no' || friend.toLowerCase() ==='n'){
       alert(' congrates your answer is correct' );
      //console.log (' congrates your answer is correct');
+     score+=1;
      document.getElementById("friend").innerText=('my friend is majdoleen  your answer is correct');
 }else{
     alert('your answer is wrong');
@@ -54,6 +59,7 @@ let barthday=prompt('my barthday is may,17 ');
 if( barthday.toLowerCase()==='yes' || barthday.toLowerCase()==='y'){
      alert( ' congrates your answer is correct' );
      //console.log ('congrates your answer is correct');
+     score+=1;
      document.getElementById("barthday").innerText=('my barthday is may,17  your answer is correct');
 }else{
     alert('your answer is wrong');
@@ -61,5 +67,58 @@ if( barthday.toLowerCase()==='yes' || barthday.toLowerCase()==='y'){
     document.getElementById("barthday").innerText=('my barthday is may,17  your answer is not correct');
 }
 
-alert (userName+' hope you enjoyed playing with us ')
+
+for(let i=0;i<4;i++){
+
+    let Siblings=parseInt(prompt('can you guess how many siblings I have?'));
+    //console.log(Siblings);
+
+    if(Siblings===3){
+        alert('congrates your answer is correct'); 
+        score+=1;
+        document.getElementById("siblings").innerText=('I have 3 siblings your answer is correct');
+        break;
+
+    }else{
+        
+        if(Siblings<3){
+            alert('too low');
+            document.getElementById("siblings").innerText=('I have 3 siblings your answer is not correct');
+
+        }else{
+        alert('too high');
+        document.getElementById("siblings").innerText=('I have 3 siblings your answer is not correct');
+        }
+    }   
+}
+alert('correct answer is 3');
+
+let hobby=['yoga','driving','watching movies'];
+
+ for (let i=0;i<6;i++){
+     let hobbies=prompt('my hobbies are');
+     //console.log(hobbies);
+
+     for(let j=0;j<3;j++){
+
+     if(hobbies.toLowerCase()===hobby[j]){
+        alert('congrates your answer is correct');
+        score+=1;
+        document.getElementById("hobbies").innerText=('my hobbies are yoga, driving, watching movies  your answer is correct');
+        i=6;
+    }
+    }if(i!=6){ 
+    alert( 'your answer is not correct please try again');
+    document.getElementById("hobbies").innerText=('my hobbies are yoga, driving, watching movies  your answer is not correct');
+    
+    }
+    }
+    alert('correct answers are: yoga, driving, watching movies' );
+
+alert (userName+' hope you enjoyed playing with us ');
+
+alert ('your score is '+ score);
+
+
+
 
