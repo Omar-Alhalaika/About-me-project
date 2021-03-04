07e523bb5f4388db5a3c05f217d398ceeb5f4fd2
Omar Alhalaika <omar.alhalaika@yahoo.com>
2021-03-04 16:45:30 +0200

    modifecation on the first question-adding a fun.Q2-Q3-Q4-Q5

--- js/app.js
+++ js/app.js
@@ -27,51 +27,73 @@ function question1(firstInput) {
 
 question1(theName);
 
-// let color=prompt('my favourite color is pink');
-//  if(color.toLowerCase() ==='no' || color.toLowerCase() ==='n'){
-//      alert(' congrates your answer is correct' );
-//       //console.log (' congrates your answer is correct');
-//       score+=1;
-//       document.getElementById("color").innerText=('my favourite color is blue  your answer is correct');
-// }else{
-//      alert('your answer is wrong');
-//      //console.log('your answer is wrong');
-//      document.getElementById("color").innerText=('my favourite color is blue  your answer is not correct');
+
+let color=prompt('my favourite color is pink');
+function question2(secondInput) {
+    if(color.toLowerCase() ==='no' || color.toLowerCase() ==='n'){
+        alert(' congrates your answer is correct' );
+         //console.log (' congrates your answer is correct');
+         score+=1;
+         document.getElementById("color").innerText=('my favourite color is blue  your answer is correct');
+   }else{
+        alert('your answer is wrong');
+        //console.log('your answer is wrong');
+        document.getElementById("color").innerText=('my favourite color is blue  your answer is not correct');
+       
+   }
+}
+question2(color);
+
+let singer=prompt('my favourite singer is sia');
+
+function question3 (thirdInput) {
+    if( singer.toLowerCase()==='yes' || singer.toLowerCase() ==='y'){
+        alert(' congrates your answer is correct' );
+         //console.log (' congrates your answer is correct');
+         score+=1;
+       document.getElementById("singer").innerText=('my favourite singer is sia  your answer is correct');
+   }else{
+       alert('your answer is wrong');
+        //console.log('your answer is wrong');
+        document.getElementById("singer").innerText=('my favourite singer is sia your answer is not correct');
+   }
     
-// }
-// let singer=prompt('my favourite singer is sia');
-// if( singer.toLowerCase()==='yes' || singer.toLowerCase() ==='y'){
-//      alert(' congrates your answer is correct' );
-//       //console.log (' congrates your answer is correct');
-//       score+=1;
-//     document.getElementById("singer").innerText=('my favourite singer is sia  your answer is correct');
-// }else{
-//     alert('your answer is wrong');
-//      //console.log('your answer is wrong');
-//      document.getElementById("singer").innerText=('my favourite singer is sia your answer is not correct');
-// }
-// let friend=prompt('sama is my friend ');
-//  if( friend.toLowerCase() ==='no' || friend.toLowerCase() ==='n'){
-//       alert(' congrates your answer is correct' );
-//      //console.log (' congrates your answer is correct');
-//      score+=1;
-//      document.getElementById("friend").innerText=('my friend is majdoleen  your answer is correct');
-// }else{
-//     alert('your answer is wrong');
-//     //console.log('your answer is wrong');
-//     document.getElementById("friend").innerText=('my friend is majdoleen  your answer is not correct');
-// }
-// let barthday=prompt('my barthday is may,17 ');
-// if( barthday.toLowerCase()==='yes' || barthday.toLowerCase()==='y'){
-//      alert( ' congrates your answer is correct' );
-//      //console.log ('congrates your answer is correct');
-//      score+=1;
-//      document.getElementById("barthday").innerText=('my barthday is may,17  your answer is correct');
-// }else{
-//     alert('your answer is wrong');
-//     //console.log('your answer is wrong');
-//     document.getElementById("barthday").innerText=('my barthday is may,17  your answer is not correct');
-// }
+}
+question3(singer);
+
+let friend=prompt('sama is my friend ');
+function question4(fourthInput) {
+    if( friend.toLowerCase() ==='no' || friend.toLowerCase() ==='n'){
+        alert(' congrates your answer is correct' );
+       //console.log (' congrates your answer is correct');
+       score+=1;
+       document.getElementById("friend").innerText=('my friend is majdoleen  your answer is correct');
+  }else{
+      alert('your answer is wrong');
+      //console.log('your answer is wrong');
+      document.getElementById("friend").innerText=('my friend is majdoleen  your answer is not correct');
+  }
+    
+}
+question4(friend);
+
+let barthday=prompt('my barthday is may,17 ');
+
+function question5(fifthInput) {
+    if( barthday.toLowerCase()==='yes' || barthday.toLowerCase()==='y'){
+         alert( ' congrates your answer is correct' );
+         //console.log ('congrates your answer is correct');
+         score+=1;
+         document.getElementById("barthday").innerText=('my barthday is may,17  your answer is correct');
+    }else{
+        alert('your answer is wrong');
+        //console.log('your answer is wrong');
+        document.getElementById("barthday").innerText=('my barthday is may,17  your answer is not correct');
+    }
+    
+}
+question5(barthday);
+
 
 
 // for(let i=0;i<4;i++){
